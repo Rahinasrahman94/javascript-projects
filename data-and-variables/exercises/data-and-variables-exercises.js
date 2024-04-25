760837bcefda5eeebf6bf5cff9c0fce4e96c9982
rahinasrahman <rshinasrahman94@gmail.com>
2024-04-25 05:32:03 -0700

    data-and-variables-with-spaces

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -5,15 +5,18 @@ let distanceToMarsKm = 225000000;
 let distanceToMoonKm = 38400;
 const milesPerKm = 0.621;
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+
 console.log(typeof shuttleName);
 console.log(typeof shuttleSpeedMph);
 console.log(typeof distanceToMarsKm);
 console.log(typeof distanceToMoonKm);
 console.log(typeof milesPerKm);
 // Calculate a space mission below
+
 let milesToMars = distanceToMarsKm*milesPerKm;
 let HoursToMars= milesToMars/shuttleSpeedMph;
 // Print the results of the space mission calculations below
+
 console.log(milesToMars );
 console.log(HoursToMars);
 // Calculate a trip to the moon below
