c44ed4cfbbb38bc014d380183086c6e40d2ee1fb
rahinasrahman <rshinasrahman94@gmail.com>
2024-04-25 05:03:36 -0700

    data-variables-changed

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -18,8 +18,7 @@ let HoursToMars= milesToMars/shuttleSpeedMph;
 // Print the results of the space mission calculations below
 console.log(HoursToMars);
 // Calculate a trip to the moon below
+
 let daysToMars = HoursToMars/24;
-
-
 // Print the results of the trip to the moon below
 console.log(shuttleName  + " will take " +  daysToMars +  " days to reach Mars");
