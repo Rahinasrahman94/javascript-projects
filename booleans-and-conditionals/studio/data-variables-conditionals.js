f81e2d844dc9b1fe8968bb222b54da23f7ece89d
rahinasrahman <rshinasrahman94@gmail.com>
2024-05-02 19:45:14 -0700

    studio-1

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,15 +1,23 @@
-// Initialize Variables below
-
-// add logic below to verify total number of astronauts for shuttle launch does not exceed 7
-
-// add logic below to verify all astronauts are ready
-
-// add logic below to verify the total mass does not exceed the maximum limit of 850000
-
-// add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-
-// add logic below to verify the fuel level is at 100%
-
-// add logic below to verify the weather status is clear
-
-// Verify shuttle launch can proceed based on above conditions
+const date = "Monday 2019-03-18";
+const time ="10:05:34 AM";
+let astronautCount = 8;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = 100;
+let weatherStatus = "clear";
+let preparedForLiftOff = true;
+if((astronautCount <= 7) && (astronautStatus === "ready") &&(totalMassKg <= maximumMassLimit ) && (fuelTempCelsius > -300 && fuelTempCelsius < -150)&&(fuelLevel === 100)&&(weatherStatus === "clear"))
+{
+  console.log( "Shuttle is ready for  launch")
+}
+else{
+  console.log("Shuttle cannot be launched");
+}
