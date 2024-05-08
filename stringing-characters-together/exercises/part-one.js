bb319ef8e138c532ee62c920d9f37c0c72b2d49c
rahinasrahman <rshinasrahman94@gmail.com>
2024-05-07 20:29:12 -0700

    string part-one.js

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,8 +1,17 @@
-let num = 1001;
+let num = 1234;
 
 //Returns 'undefined'.
-console.log(num.length);
-
+let num1 = num.toString();
+let result = num1.indexOf(".");
+if(result> -1)
+{
+console.log("Decimal");
+console.log(num1.length-1);
+}
+else{
+console.log("Integer");
+console.log(num1.length);
+}
 //Use type conversion to print the length (number of digits) of an integer.
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
