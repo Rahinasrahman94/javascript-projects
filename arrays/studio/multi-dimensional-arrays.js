f32f68e8e0ddb0c5b327479d97f19d0e4fbda34a
rahinasrahman <rshinasrahman94@gmail.com>
2024-05-16 17:49:14 -0700

    loop studio exercises

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -14,14 +14,18 @@ let sleepAidsCabinet =[];
 cargoHold =[food,equipment,pets,sleepAids];
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
 foodCabinet = food.split(",");
+foodCabinet.sort();
 equipmentCabinet = equipment.split(",");
+equipmentCabinet.sort();
 petsCabinet = pets.split(","); 
+petsCabinet.sort();
 sleepAidsCabinet =sleepAids.split(",");
+sleepAidsCabinet.sort();
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
 cargoHold = [foodCabinet,equipmentCabinet,petsCabinet,sleepAidsCabinet]
 cabinetName = ["foodCabinet","equipmentCabinet","petsCabinet","sleepAidsCabinet"];
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
-cabinetNumber = input.question("Please Enter the Cabinet number");
+cabinetNumber = input.question("Please Enter the Cabinet number  ");
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
 if(cabinetNumber > cargoHold.length)
@@ -36,10 +40,10 @@ else
      Items :${cargoHold[cabinetNumber]}`);
 }
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
-cabinetNumber = input.question("Please Enter the Cabinet number");
-cabinetItem = input.question("Please Enter the Item from the cabinet");
+cabinetNumber = input.question("Please Enter the Cabinet number :");
+cabinetItem = input.question("Please Enter the Item from the cabinet:");
 let str = cargoHold[cabinetNumber].includes(cabinetItem);
-console.log(str);
+//console.log(str);
 if(str)
 {
     console.log("Cabinet :"+ cabinetName[cabinetNumber] + "   DOES contain :" + cabinetItem)
--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -2,36 +2,55 @@ const input = require('readline-sync');
 
 // Part A: #1 Populate these arrays
 
-let protein = [];
-let grains = [];
-let veggies = [];
-let beverages = [];
-let desserts = [];
+let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+let veggies = ['peas', 'green beans', 'kale', 'edamame', 'broccoli', 'asparagus'];
+let beverages = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
+let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'];
 
 
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   let pantry = [protein, grains, veggies, beverages, desserts];
   let meals = [];
-  
-  /// Part A #2: Write a ``for`` loop inside this function
-  /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
-
-
-  return meals;
+   /// Part A #2: Write a ``for`` loop inside this function
+ /* for(let i=0;i<numMeals;i++)
+  {
+    //console.log("inside for loop")
+    meals[i] = [protein[i],grains[i], veggies[i], beverages[i], desserts[i]]
+  }*/
+/// Code your solution for part A #2 below this comment (and above the return statement) ... ///
+for(let i = 0;i<numMeals;i++)
+  {
+    meals[i] =[protein[i],grains[i], veggies[i], beverages[i], desserts[i]]
+  }
+ return meals;
 }
 
 
 function askForNumber() {
-  numMeals = input.question("How many meals would you like to make?");
-  
+ 
   /// CODE YOUR SOLUTION TO PART B here ///
-
+  let index = 0;
+ while(index <= 0)
+  {
+   numMeals = input.question("How many meals would you like to make?");
+   if(numMeals<=6)
+    {
   return numMeals;
+    }
+    else{
+      console.log("Please enter the number of meals between 1 and 6")
+    }
+    //index;
+}
 }
 
 
 function generatePassword(string1, string2) {
   let code = '';
+  let Firststring = input.question("Enter the First String");
+  
+
 
   /// Code your Bonus Mission Solution here ///
 
@@ -45,16 +64,16 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  // console.log(meals)
+let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+console.log(meals)
   
 
   /// TEST PART B HERE ///
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
   
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+ let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+ console.log(mealsForX);
 
     /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
